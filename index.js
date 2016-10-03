bc6ec20f960e25476871ed53e676edbe2b9add72
Nicolas Humbert <nicolas.humbert@live.fr>
2016-10-02 18:06:11 -0700

    put editWords and words together

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 'use strict'; // eslint-disable-line strict
 
 const Words = require('./Words');
-const edit = require('./editWord');
 
 module.exports = function correctMe(word, cb) {
     const words = new Words();
@@ -9,23 +8,24 @@ module.exports = function correctMe(word, cb) {
         if (err) {
             return cb(err);
         }
-
         // if the word is correct
         if ((words.known([word])).length > 0) {
             return cb(null, word);
         }
-        const candidates1 = edit.editWord(word);
+        const candidates1 = words.getCandidates(word);
         const candidatesKnown1 = words.known(candidates1);
         // if the word only need one change
         if (candidatesKnown1.length > 0) {
+            words.counter();
             const corr = words.corrections(candidatesKnown1);
             return cb(null, candidatesKnown1[corr[0]]);
         // else if the word need 2 changes
         }
-        const candidates2 = edit.editWord2(candidates1);
+        const candidates2 = words.getCandidates2(candidates1);
         // console.log('candidates2', candidates2);
         const candidatesKnown2 = words.known(candidates2);
         if (candidatesKnown2.length > 0) {
+            words.counter();
             const corr = words.corrections(candidatesKnown2);
             return cb(null, candidatesKnown2[corr[0]]);
         }
